cabd652d9c0ac32041c025d517d4dc6bb7bd0394
Ryna Truong <rynatruong@outlook.com>
2020-10-06 17:54:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,40 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Articles.all
+    erb :index
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+    
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to('/articles/#{@article.id}')
+    erb :show
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @article_edit = Article.find_by_id(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to('/articles/#{@articcle.id}')
+  end
+  
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to('articles')
+  end
+    
 end
